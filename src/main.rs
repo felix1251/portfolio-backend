4f70ab6acdd12ae75c86ba70bac230aae8f12e27
felix1251 <domhack123@gmail.com>
2023-07-12 18:27:46 +0800

    setup actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer};
+
+//home path and serves as health checker
+#[get("/")]
+async fn index() -> String {
+    "this is a health check".to_string()
+}
+
+//main actix web server to run
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8081))?
+    .run()
+    .await
 }
