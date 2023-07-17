3243e1bbec30381f8ad26229dcf9d09a75f79ecb
felix1251 <domhack123@gmail.com>
2023-07-17 22:23:24 +0800

    create blogs table

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,10 +1,12 @@
 pub use sea_orm_migration::prelude::*;
 
+mod m20230717_141821_create_blogs_table;
+
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![]
+        vec![Box::new(m20230717_141821_create_blogs_table::Migration)]
     }
 }
